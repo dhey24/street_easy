b01121b529de41bda744e0b721c299d9aef51cb8
David Hey <davidhey@me.com>
2017-10-13 12:15:24 -0700

    Adding script to query mongo and write to csv for tableau

---
+++ query_mongo.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pymongo
+from secret import MONGO_USER, MONGO_PW
+from mongo import mongo_connect
+import pprint
+
+def main():
+	coll = mongo_connect(MONGO_USER, MONGO_PW)
+	listings = coll.find({"no_fee": {"$regex": u"NO"}}).limit(10)
+	df = pd.DataFrame(list(listings))
+	#pprint.pprint(listings[0])
+	print df.head()
+	df.to_csv("./mongo_listings.csv")
+
+if __name__ == '__main__':
+	main()
